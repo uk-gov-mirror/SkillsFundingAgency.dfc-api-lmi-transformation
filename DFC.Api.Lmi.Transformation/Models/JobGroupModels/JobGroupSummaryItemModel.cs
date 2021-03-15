5ce1fffaa9071d84c3ef889697beef08898d9def
Ian Crisp <icrisp@program-systems.co.uk>
2021-03-15 17:19:17 +0000

    Updated get details and get summary list to use ID instead of Soc

--- DFC.Api.Lmi.Transformation/Models/JobGroupModels/JobGroupSummaryItemModel.cs
+++ DFC.Api.Lmi.Transformation/Models/JobGroupModels/JobGroupSummaryItemModel.cs
@@ -1,10 +1,13 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace DFC.Api.Lmi.Transformation.Models.JobGroupModels
 {
     [ExcludeFromCodeCoverage]
     public class JobGroupSummaryItemModel
     {
+        public Guid? Id { get; set; }
+
         public int Soc { get; set; }
 
         public string? Title { get; set; }
